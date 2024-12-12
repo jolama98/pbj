845706e8c0c363dca830a0528b9ffa0209ab6349
Autumn Eckley <eckley.autumn@gmail.com>
2024-12-11 22:17:52 -0700

    Saving poems to the AppState

--- client/src/services/PoemsService.js
+++ client/src/services/PoemsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Poem } from "@/models/Poem.js"
+import { AppState } from "@/AppState.js"
 
 class PoemsService {
   async getAllPoems() {
     const response = await api.get('api/poem')
     logger.log('Got all poems - poems service', response.data)
+    const newPoems = response.data.map(poemData => new Poem(poemData))
+    AppState.poems = newPoems
   }
 
 

5917dd1a8ec8af7cede642e181bc15503d33b568
jo <josehehall98@gmail.com>
2024-12-20 18:49:15 -0700

    Added isArchived on create poem from

--- client/src/components/CreatePoemModal.vue
+++ client/src/components/CreatePoemModal.vue
@@ -46,14 +46,20 @@ async function createPoem() {
     </div>
 
     <div class="mb-3">
-      <label for="tagsTitle" class="form-label fw-bold fs-3">Tags</label>
+      <label for="poemBody" class="form-label fw-bold">Body</label>
+      <textarea v-model="poemData.body" type="text" class="form-control" id="poemBody" required
+        maxlength="2000"></textarea>
+    </div>
+
+    <div class="mb-3">
+      <label for="tagsTitle" class="form-label">Tags</label>
       <input v-model="poemData.tags" type="text" class="form-control" id="tagsTitle" aria-describedby="tagsHelp"
         required maxlength="20">
     </div>
+
     <div class="mb-3">
-      <label for="poemBody" class="form-label">Body</label>
-      <textarea v-model="poemData.body" type="text" class="form-control" id="poemBody" required
-        maxlength="2000"></textarea>
+      <input type="checkbox" class="form-check-input m-1" id="isPoemArchived" v-model="poemData.isArchived">
+      <label class="form-check-label" for="isPoemArchived">Is poem Archived?</label>
     </div>
 
     <div class="col-12 mb-3">

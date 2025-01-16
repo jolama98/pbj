c09963562caee7a652bd0ea913627271e885e26b
jo <josehehall98@gmail.com>
2025-01-16 13:20:05 -0700

    update poem likes on backend and see them change on the frontend

--- server/Services/LikedPoemService.cs
+++ server/Services/LikedPoemService.cs
@@ -5,20 +5,24 @@ namespace pbj.Services;
 public class LikedPoemService
 {
     private readonly LikedPoemRepository _likedPoemRepository;
+    private readonly PoemRepository _poemRepository;
+
     private readonly PoemService _poemService;
     private readonly AccountService _accountService;
 
-    public LikedPoemService(PoemService poemService, LikedPoemRepository likedPoemRepository, AccountService accountService)
+    public LikedPoemService(PoemService poemService, LikedPoemRepository likedPoemRepository, AccountService accountService, PoemRepository poemRepository)
     {
         _poemService = poemService;
         _likedPoemRepository = likedPoemRepository;
         _accountService = accountService;
+        _poemRepository = poemRepository;
     }
 
     internal LikedPoem CreateLikedPoem(LikedPoem likedPoemData, string userId)
     {
         Poem poem = _poemService.GetPoemById(likedPoemData.PoemId, userId);
         poem.Likes += 1;
+        _poemRepository.UpdatePoem(poem);
 
         LikedPoem likedPoem = _likedPoemRepository.CreateLikedPoem(likedPoemData);
         return likedPoem;
